8793d87f693af0c6d8013ed301496116d52d60e6
The5-1 <agaku@hotmail.de>
2018-01-12 18:38:23 +0100

    alternate IK implementation removed from project

---
+++ AssetsExcluded/IKBoneSimple.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKBoneSimple : MonoBehaviour
+{
+    public string name;
+
+    public float length;
+    public float width;
+    public float thickness;
+
+    IKBoneSimple parentBone;
+    List<IKBoneSimple> childBones;
+
+    public Rigidbody physicsRB;
+    public Collider physicsCollider;
+    public Joint physicsJoint;
+    public GameObject attachmentGO;
+
+    private void Awake()
+    {
+        if (false)
+        {
+            this.physicsRB = this.gameObject.AddComponent<Rigidbody>();
+            this.physicsCollider = this.gameObject.AddComponent<BoxCollider>();
+            this.physicsJoint = this.gameObject.AddComponent<CharacterJoint>();
+        }
+
+        if (true)
+        {
+            attachmentGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            attachmentGO.GetComponent<Collider>().enabled = false;
+            attachmentGO.transform.parent = this.transform;
+            attachmentGO.transform.localScale = new Vector3(width, length, thickness);
+            attachmentGO.transform.localRotation = Quaternion.identity;
+            attachmentGO.transform.localPosition = new Vector3(0.0f, length * 0.5f, 0.0f);
+        }
+    }
+
+    void init(string name, float length, float width, float thickness, IKBoneSimple parentBone)
+    {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+        this.thickness = thickness;
+        this.parentBone = parentBone;
+    }
+
+    void refresh()
+    {
+
+    }
+
+}
---
+++ AssetsExcluded/IKSkeleton2.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKTarget2 : MonoBehaviour
+{
+    public IKChainBase chain;
+
+    public void init(IKChainBase chain)
+    {
+        this.chain = chain;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(this.transform.position, Vector3.one * (0.1f + float.Epsilon));
+    }
+}
+
+
+public abstract class IKChainBase
+{
+    public string name;
+    public readonly float maxLength;
+
+    protected GameObject root; //the object this chain is attached to
+    protected List<IKBoneSimple> bones;
+    protected IKTarget2 target; //the target to stretch to
+
+    protected IKChainBase(string name)
+    {
+        this.name = name;
+    }
+
+    public abstract void Update();
+
+    public abstract float UpdateMaxLength();
+
+    public void attach(GameObject parent)
+    {
+        first.gameObject.transform.parent = parent.transform;
+    }
+
+}
+
+public class IKChainTwoBone : IKChainBase
+{
+    private IKBoneSimple second;
+
+    public Transform hint;
+
+    protected IKChainTwoBone(string name) : base(name)
+    {
+        GameObject first_GO = new GameObject(name + "_first");
+        first = first_GO.AddComponent<IKBoneSimple>();
+        GameObject second_GO = new GameObject(name + "_second");
+        second = second_GO.AddComponent<IKBoneSimple>();
+
+        GameObject target_GO = new GameObject(name + "_target");
+        target = target_GO.AddComponent<IKTarget2>();
+        target.init(this);
+    }
+
+    public override void Update()
+    {
+        if (root == null || target == null || first == null || second == null) return;
+
+    }
+
+    public override float UpdateMaxLength()
+    {
+        return 1.0f;
+    }
+}
+
+
+public class IKSkeleton2 : MonoBehaviour {
+
+    List<IKChainBase> ikChains;
+
+
+    private void Awake()
+    {
+        ikChains = new List<IKChainBase>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
